08b255c029ea61320d014a78159c93d7f0c8c621
Amarnath Reddy <tamarnathr@gmail.com>
2025-05-14 07:56:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function fibonacci(num) {
-    if (num === 1) return 0;
-    if (num === 2) return 1;
 
-    let a = 0, b = 1;
-    for (let i = 3; i <= num; i++) {
-        let temp = a + b;
-        a = b;
-        b = temp;
-    }
-    return b;
-}
